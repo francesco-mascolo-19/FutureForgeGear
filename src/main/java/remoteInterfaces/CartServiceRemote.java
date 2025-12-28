f70f1a752f09abe51dc2d02c0a648580ba0dd920
FrancescoMascolo19 <francescomascolo03.fm@gmail.com>
2025-12-28 17:35:28 +0100

    Integrazione del database e aggiornamento dei file

--- src/main/java/remoteInterfaces/CartServiceRemote.java
+++ src/main/java/remoteInterfaces/CartServiceRemote.java
@@ -2,10 +2,12 @@ package remoteInterfaces;
 
 import model.Cart;
 
+//import javax.ejb.Remote;
 import jakarta.ejb.Remote;
 
 @Remote
 public interface CartServiceRemote {
+
     void addCart(Cart cart);
     void clearCart(Cart cart);
     void updateCart(Cart cart);
