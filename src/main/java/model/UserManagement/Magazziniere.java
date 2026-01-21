b7096f90854b3f5369b4d36cab8be848952a4add
francesco-mascolo-19 <f.mascolo19@studenti.unisa.it>
2026-01-21 14:15:53 +0100

    Modifiche varie relative agli utenti legati al sito

--- src/main/java/model/UserManagement/Magazziniere.java
+++ src/main/java/model/UserManagement/Magazziniere.java
@@ -1,27 +1,28 @@
 package model.UserManagement;
 
+import enumerativeTypes.Ruolo;
 import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import model.OrderManagement.Prodotto;
-import model.UserManagement.Magazzino;
-import java.util.List;
+
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @DiscriminatorValue("MAGAZZINIERE")
 public class Magazziniere extends Utente{
 
-    @Embedded
     private Magazzino magazzino;
 
     public Magazziniere() {}
+
     // Constructor with basic fields
-    public Magazziniere(String nome, String cognome, String email, String username, String password, Magazzino magazzino) {
-        super(nome, cognome, email, username, password);
+    public Magazziniere(String nome, String cognome, String email, String password, Magazzino magazzino) {
+        super(nome, cognome, email, password, Ruolo.MAGAZZINIERE);
+        this.magazzino = magazzino;
+    }
 
     public Magazzino getMagazzino() {return magazzino;}
-
     public void setMagazzino(Magazzino magazzino) {this.magazzino = magazzino;}
 
     @Override
